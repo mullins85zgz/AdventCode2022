ecfe5af1100a9fa2a4c84b042ad8e186e489e0e3
Jose Angel "Casa <jangelsalinas@gmail.com>
2022-12-04 16:35:23 +0100

    Day 2 completed!

---
+++ src/day2/Day2_1.java
@@ -0,0 +1,106 @@
+package day2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Day2_1 {
+
+    //Indexes
+    final static int ROCK = 0;
+    final static int PAPER = 1;
+    final static int SCISSORS = 2;
+
+    final static String[] JUGADAS_RIVALES = {"A","B","C"};
+    final static String[] JUGADAS_PROPIAS = {"X","Y","Z"};
+    final static int[] PUNTUACION = {1,2,3};
+
+    static int [][] tablaResultados =  null;
+
+
+
+    final static int WIN_SCORE = 6;
+    final static int DRAW_SCORE = 3;
+    final static int LOSE_SCORE = 0;
+    public static void main(String[] args) {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        // Definicion de las jugadas, donde el primer elemento del array es la jugada nuestra y el segundo la jugada rival
+        tablaResultados = new int[][]{
+                {DRAW_SCORE, LOSE_SCORE, WIN_SCORE  },
+                {WIN_SCORE, DRAW_SCORE, LOSE_SCORE},
+                {LOSE_SCORE, WIN_SCORE, DRAW_SCORE}
+        };
+
+        int aux = 0;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File ("D:\\input2.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while((linea=br.readLine())!=null){
+                if(linea.trim().length()!=0){
+                    String jugadaRival = linea.substring(0,1);
+                    String jugadaPropia = linea.substring(2,3);
+//                    System.out.println(jugadaRival + "-"  + jugadaPropia);
+                    aux+=calculatePlayScore(jugadaRival,jugadaPropia);
+                }
+            }
+
+            System.out.println(aux);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta
+            // una excepcion.
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    public static int calculatePlayScore (String jugadaRival, String jugadaPropia){
+        int score = 0;
+
+        int posicionJugadaPropia = findIndex(JUGADAS_PROPIAS,jugadaPropia);
+        int posicionJugadaRival = findIndex(JUGADAS_RIVALES,jugadaRival);
+
+        // Player score based on election
+        int chooseScore  = PUNTUACION[posicionJugadaPropia];
+        score += chooseScore;
+
+        //Result of the play
+        int playScore = tablaResultados[posicionJugadaPropia][posicionJugadaRival];
+        score += playScore;
+
+        return score;
+    }
+
+    public static int findIndex(String arr[], String t)
+    {
+        // Creating ArrayList
+        ArrayList<String> clist = new ArrayList<>();
+
+        // adding elements of array
+        // to ArrayList
+        for (String i : arr)
+            clist.add(i);
+
+        // returning index of the element
+        return clist.indexOf(t);
+    }
+}
---
+++ src/day2/Day2_2.java
@@ -0,0 +1,119 @@
+package day2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Day2_2 {
+
+    //Indexes
+    final static int ROCK = 0;
+    final static int PAPER = 1;
+    final static int SCISSORS = 2;
+
+    final static String[] JUGADAS_RIVALES = {"A","B","C"};
+    final static String[] JUGADAS_PROPIAS = {"X","Y","Z"};
+    final static int[] PUNTUACION = {1,2,3};
+
+    final static int[] PUNTUACION_JUGADA = {0,3,6};
+
+    static int [][] tablaResultados =  null;
+
+
+
+    final static int WIN_SCORE = 6;
+    final static int DRAW_SCORE = 3;
+    final static int LOSE_SCORE = 0;
+    public static void main(String[] args) {
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        // Definicion de las jugadas, donde el primer elemento del array es la jugada rival y el segundo la jugada nuestra (al reves que el dia 2-1)
+        tablaResultados = new int[][]{
+                {DRAW_SCORE, WIN_SCORE,  LOSE_SCORE },
+                {LOSE_SCORE, DRAW_SCORE, WIN_SCORE},
+                {WIN_SCORE, LOSE_SCORE, DRAW_SCORE}
+        };
+
+        int aux = 0;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File ("D:\\input2.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while((linea=br.readLine())!=null){
+                if(linea.trim().length()!=0){
+                    String jugadaRival = linea.substring(0,1);
+                    String jugadaPropia = linea.substring(2,3);
+//                    System.out.println(jugadaRival + "-"  + jugadaPropia);
+                    aux+=calculatePlayScore(jugadaRival,jugadaPropia);
+                }
+            }
+
+            System.out.println(aux);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta
+            // una excepcion.
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    public static int calculatePlayScore (String jugadaRival, String resultadoForzado){
+        int score = 0;
+
+        int posicionResultadoForzado = findIndex(JUGADAS_PROPIAS,resultadoForzado);
+        int posicionJugadaRival = findIndex(JUGADAS_RIVALES,jugadaRival);
+
+        // Segun resultado esperado sumamos
+        int playScore  = PUNTUACION_JUGADA[posicionResultadoForzado];
+        score += playScore;
+
+        //Ahora, debemos saber que eleccion hacemos, y segun la misma sumamos
+        int eleccion = devuelveJugada(tablaResultados[posicionJugadaRival],PUNTUACION_JUGADA[posicionResultadoForzado]);
+        int chooseScore  = PUNTUACION[eleccion];
+        score += chooseScore;
+
+        return score;
+    }
+
+    public static int findIndex(String arr[], String t)
+    {
+        // Creating ArrayList
+        ArrayList<String> clist = new ArrayList<>();
+
+        // adding elements of array
+        // to ArrayList
+        for (String i : arr)
+            clist.add(i);
+
+        // returning index of the element
+        return clist.indexOf(t);
+    }
+
+    public static int devuelveJugada(int[] rival, int resultadoForzado){
+        int posicionJugada = -1;
+
+        for(int i=0; i < rival.length;i++){
+            if(resultadoForzado==rival[i])  return i;   //Devolvemos el indice del resultado esperado vs el del rival para saber cuanto vale la jugada
+        }
+
+        return posicionJugada;
+    }
+}
